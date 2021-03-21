84b27f82e037ec2fea380d37ce1d6b0295901948
anamarta49 <fc49562@alunos.fc.ul.pt>
2021-03-21 13:58:51 +0000

    small changes

--- mysqlCompanyQueries.sql
+++ mysqlCompanyQueries.sql
@@ -2,6 +2,9 @@
 -- 1.Write a query to display the name (first_name and last_name) and
 -- department ID of all employees in departments 30 or 100 in ascending order.
 SELECT CONCAT(first_name, " ", last_name) AS name, department_id
-FROM `employees`
+FROM employees
 WHERE (department_id=30 OR department_id=100)
 ORDER BY name;
+
+-- 2. Write a query to find the manager ID and the salary of the lowest-paid
+-- employee for that manager.
