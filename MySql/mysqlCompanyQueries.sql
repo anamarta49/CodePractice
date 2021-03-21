0ab7b199ffd4dc0f428660afd7aaa584811863ee
anamarta49 <a.marta.gato@hotmail.com>
2021-03-21 17:53:16 +0000

    3rd query exercise done

--- MySql/mysqlCompanyQueries.sql
+++ MySql/mysqlCompanyQueries.sql
@@ -11,3 +11,11 @@ ORDER BY name;
 SELECT manager_id, MIN(salary) AS lowest_employee_sal
 FROM employees
 GROUP BY manager_id;
+
+-- 3. Write a query to find the name (first_name and last_name) and the salary
+-- of the employees who earn more than the employee whose last name is Bell.
+SELECT CONCAT(first_name, " ", last_name) AS name, salary
+FROM employees
+WHERE salary>(SELECT salary
+              FROM employees
+              WHERE last_name='Bell');
